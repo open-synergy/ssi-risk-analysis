4f535ec5d15ac3625c66f72e7fd57d80ddd37569
usa8bit <usa8bit@gmail.com>
2023-10-24 13:58:21 +0700

    [ssi_risk_analysis_related_attachment] 14.0.1.0.0

--- ssi_risk_analysis_related_attachment/models/__init__.py
+++ ssi_risk_analysis_related_attachment/models/__init__.py
@@ -1,8 +1,7 @@
-# Copyright 2022 OpenSynergy Indonesia
-# Copyright 2022 PT. Simetri Sinergi Indonesia
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
     risk_analysis,
-    risk_analysis_worksheet
 )
